4c16dfdb39d8cab617354f100c09e4aaad5e6a16
Alisa <alisabox@mail.ru>
2021-06-26 14:53:10 +0900

    Добавляет карту

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-import {generatePublications} from './data.js';
-import {createCard} from './card.js';
-import {setInactiveState, setActiveState} from './form-state.js';
+import './map.js';
 import './form-validation.js';
 
-const PUBLICATIONS_COUNT = 10;
-const publications = generatePublications(PUBLICATIONS_COUNT);
-
-const cards = publications.map((publication) => createCard(publication));
-
-const mapCanvas = document.querySelector('.map__canvas');
-mapCanvas.append(cards[0]);
-
-setInactiveState();
-setActiveState();
